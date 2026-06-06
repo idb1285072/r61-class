667b735d715ff77559ad90be213b9c8118df97ba
idb1285072 <idb.1285072@gmail.com>
2026-06-06 12:47:02 +0600

    2025-01-29

---
+++ R61_1285072/R61_1285072/Models/DbInvContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace R61_1285072.Models
+{
+    public class DbInvContext : IdentityDbContext
+    {
+        public DbInvContext() : base("DbInvContext")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sales> Sales { get; set; }
+        public DbSet<Details> Details { get; set; }
+    }
+    public class Product
+    {
+        public int ID { get; set; }
+        [Required, MaxLength(128)]
+        public string Name { get; set; }
+    }
+    public class Sales
+    {
+        public int ID { get; set; }
+        [Required, MaxLength(20, ErrorMessage = "Name is must")]
+        public string Name { get; set; }
+        [Required, DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; }
+        [MaxLength(200)]
+        public string Picture { get; set; }
+
+        public bool Status { get; set; }
+        public List<Details> Details { get; set; }
+
+    }
+    public class Details
+    {
+        public int ID { get; set; }
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        [ForeignKey(nameof(Sales))]
+        public int OrderId { get; set; }
+        [Required, DataType(DataType.Currency)]
+        public double Price { get; set; }
+        public Product Product { get; set; }
+        public Sales Sales { get; set; }
+    }
+}
---
+++ R61_1285072/R61_1285072/Provider/APIAUTHORIZATIONSERVERPROVIDER.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.OAuth;
+using R61_1285072.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace R61_1285072.Provider
+{
+    public class APIAUTHORIZATIONSERVERPROVIDER : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            // return base.ValidateClientAuthentication(context);
+            context.Validated();
+        }
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            // return base.GrantResourceOwnerCredentials(context);
+            var userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(new DbInvContext()));
+            IdentityUser user = await userManager.FindAsync(context.UserName, context.Password);
+
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+            context.Validated(identity);
+        }
+    }
+}
